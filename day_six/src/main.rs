b13ebb07b9f7f912f8da16edd5e14d9a513c68e7
JohnKossa <kg959ster@gmail.com>
2022-12-06 01:34:20 -0600

    Day 6: Removed an unneeded function call to save 0.3ms

--- day_six/src/main.rs
+++ day_six/src/main.rs
@@ -8,14 +8,10 @@ fn main() {
     println!("Execution time: {:?}", now.elapsed());
 }
 
-fn test_unique(test_str: String) -> bool{
-    test_str.chars().all_unique()
-}
-
 fn part_1(){
     let input = fs::read_to_string("files/input.txt").expect("Should have been able to read the file");
     for last_idx in 4..input.chars().count(){
-        if test_unique(input[last_idx-4..last_idx].to_string()){
+        if input[last_idx-4..last_idx].chars().all_unique(){
             println!("Last index for pt1 is: {}", last_idx+1);
             break;
         }
@@ -25,7 +21,7 @@ fn part_1(){
 fn part_2(){
     let input = fs::read_to_string("files/input.txt").expect("Should have been able to read the file");
     for last_idx in 14..input.chars().count(){
-        if test_unique(input[last_idx-14..last_idx].to_string()){
+        if input[last_idx-14..last_idx].chars().all_unique(){
             println!("Last index for pt2 is: {}", last_idx+1);
             break;
         }
