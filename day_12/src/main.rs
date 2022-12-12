69a1ba7461bbe96c3ba2e8c88307a429fd0a9a07
JohnKossa <kg959ster@gmail.com>
2022-12-12 01:50:23 -0600

    Day 12 style pass

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -7,24 +7,31 @@ fn main() {
     let mut height_map:Vec<Vec<u32>> = Vec::new();
     let mut start:(usize, usize) = (0,0);
     let mut end:(usize, usize) = (0,0);
-    let mut lines = contents.trim().split("\n");
+    let mut lines = contents
+        .trim()
+        .split("\n");
     let mut row_num: usize = 0;
     while let Some(line) = lines.next(){
-        height_map.push(line.trim().chars().enumerate().map(|(idx, c)|{
-            match (idx, c){
-                (x, 'S') => {
-                    start = (x, row_num);
-                    0
-                },
-                (x, 'E')=>{
-                    end = (x, row_num);
-                    27
-                },
-                (_,a)=>{
-                    (a as u32)-96
-                }
-            }
-        }).collect::<Vec<u32>>());
+        height_map.push(line
+                        .trim()
+                        .chars()
+                        .enumerate()
+                        .map(|(idx, c)|{
+                            match (idx, c){
+                                (x, 'S') => {
+                                    start = (x, row_num);
+                                    0
+                                },
+                                (x, 'E')=>{
+                                    end = (x, row_num);
+                                    27
+                                },
+                                (_,a)=>{
+                                    (a as u32)-96
+                                }
+                            }
+                        })
+                        .collect::<Vec<u32>>());
         row_num+=1;
     }
     part_1(&height_map, start);
@@ -72,18 +79,20 @@ fn part_1(height_map: &Vec<Vec<u32>>, start: Coordinate){
             println!("Answer for part 1: {}", current_dist);
             break;
         }
-        let mut neighbors = neighbors_of((x, y), grid_width, grid_height);
-        neighbors = neighbors.iter().filter(|coord|{
-            !visited[coord.1 ][coord.0 ]
-        }).filter(|coord|{
-            let coord_height = height_map[coord.1][coord.0];
-            coord_height <= current_height || coord_height-current_height == 1
-        }).map(|x|*x).collect();
-        neighbors.iter().for_each(|coord|{
-            visited[y][x] = true;
-            dist_map[coord.1][coord.0] = current_dist+1;
-            paths.push_back((coord.0, coord.1));
-        });
+        let neighbors: Vec<Coordinate> = neighbors_of((x, y), grid_width, grid_height)
+            .iter()
+            .filter(|coord|!visited[coord.1][coord.0])
+            .filter(|coord|{
+                let coord_height = height_map[coord.1][coord.0];
+                coord_height <= current_height || coord_height-current_height == 1
+            }).map(|x|*x).collect();
+        neighbors
+            .iter()
+            .for_each(|coord|{
+                visited[y][x] = true;
+                dist_map[coord.1][coord.0] = current_dist+1;
+                paths.push_back((coord.0, coord.1));
+            });
         paths = VecDeque::from(paths.iter().unique().map(|x|*x).collect::<Vec<Coordinate>>());
     }
 }
@@ -105,18 +114,27 @@ fn part_2(height_map: &Vec<Vec<u32>>, end: Coordinate){
             println!("Answer for part 2: {}", current_dist);
             break;
         }
-        let mut neighbors = neighbors_of((x, y), grid_width, grid_height);
-        neighbors = neighbors.iter().filter(|coord|{
-            !visited[coord.1][coord.0]
-        }).filter(|coord|{
-            let coord_height = height_map[coord.1][coord.0];
-            current_height <= coord_height|| current_height - coord_height == 1
-        }).map(|x|*x).collect();
-        neighbors.iter().for_each(|coord|{
-            visited[y][x] = true;
-            dist_map[coord.1][coord.0] = current_dist+1;
-            paths.push_back((coord.0, coord.1));
-        });
-        paths = VecDeque::from(paths.iter().unique().map(|x|*x).collect::<Vec<Coordinate>>());
+        let neighbors: Vec<Coordinate> = neighbors_of((x, y), grid_width, grid_height)
+            .iter()
+            .filter(|coord|{
+                !visited[coord.1][coord.0]
+            })
+            .filter(|coord|{
+                let coord_height = height_map[coord.1][coord.0];
+                current_height <= coord_height|| current_height - coord_height == 1
+            })
+            .map(|x|*x).collect();
+        neighbors
+            .iter()
+            .for_each(|coord|{
+                visited[y][x] = true;
+                dist_map[coord.1][coord.0] = current_dist+1;
+                paths.push_back((coord.0, coord.1));
+            });
+        paths = VecDeque::from(paths
+                                .iter()
+                                .unique()
+                                .map(|x|*x)
+                                .collect::<Vec<Coordinate>>());
     }
 }
